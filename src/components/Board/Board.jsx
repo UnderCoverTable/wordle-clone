8a85549965c2319d74e45e7be2541be7060c61f7
UnderCoverTable <sameed.gilani@gmail.com>
2025-06-17 21:00:20 +0500

    fixed div focus

--- src/components/Board/Board.jsx
+++ src/components/Board/Board.jsx
@@ -1,9 +1,14 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import Card from "../Card/Card";
 import { handleBackspace, handleEnter, handleLetter } from "./Helpers";
 
 export default function Board({ dimension = 5, todaysWord = [] }) {
+  const focusDivRef = useRef(null);
   const [guessStore, setGuessStore] = useState({});
+
+  useEffect(() => {
+    focusDivRef.current?.focus();
+  }, []);
 
   useEffect(() => {
     const updatedStore = {};
@@ -20,44 +25,58 @@ export default function Board({ dimension = 5, todaysWord = [] }) {
   }, [dimension]);
 
   return (
-    <div style={{ display: "flex", flexDirection: "column", gap: 10 }}>
-      {Object.keys(guessStore).map((row) => {
-        const currentRow = guessStore[row];
+    <div>
+      {/* Invisible div that listens for typing */}
+      <div
+        ref={focusDivRef}
+        tabIndex={0}
+        onBlur={() => {
+          focusDivRef.current?.focus();
+        }}
+        onKeyDown={(event) => {
+          const letter = event.key;
 
-        return (
-          <div style={{ display: "flex", flexDirection: "row", gap: 10 }}>
-            {Array.from({ length: currentRow.row.length }).map((_, i) => {
-              return (
-                <div
-                  tabIndex={0}
-                  onKeyDown={(event) => {
-                    const letter = event.key;
+          if (/^[a-zA-Z]$/.test(letter)) {
+            handleLetter({ letter, guessStore, setGuessStore });
+          } else if (letter === "Backspace") {
+            handleBackspace({ guessStore, setGuessStore });
+          } else if (letter === "Enter") {
+            handleEnter({
+              answer: todaysWord,
+              guessStore,
+              setGuessStore,
+            });
+          }
+        }}
+        style={{
+          position: "absolute",
+          opacity: 0,
+          pointerEvents: "none",
+        }}
+      />
 
-                    if (/^[a-zA-Z]$/.test(letter)) {
-                      handleLetter({ letter, guessStore, setGuessStore });
-                    } else if (letter === "Backspace") {
-                      handleBackspace({ guessStore, setGuessStore });
-                    } else if (letter === "Enter") {
-                      handleEnter({
-                        answer: todaysWord,
-                        guessStore,
-                        setGuessStore,
-                      });
-                    }
-                  }}
-                >
-                  <Card
-                    key={i}
-                    index={i}
-                    guess={currentRow?.row?.[i]}
-                    guessStatus={currentRow?.rowStatuses?.[i]}
-                  />
-                </div>
-              );
-            })}
-          </div>
-        );
-      })}
+      {/* Game board */}
+      <div style={{ display: "flex", flexDirection: "column", gap: 10 }}>
+        {Object.keys(guessStore).map((row) => {
+          const currentRow = guessStore[row];
+
+          return (
+            <div
+              key={row}
+              style={{ display: "flex", flexDirection: "row", gap: 10 }}
+            >
+              {currentRow.row.map((_, i) => (
+                <Card
+                  key={i}
+                  index={i}
+                  guess={currentRow?.row?.[i]}
+                  guessStatus={currentRow?.rowStatuses?.[i]}
+                />
+              ))}
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
