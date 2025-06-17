e0bd041bbb239abb163934f320dbdc204df890f7
UnderCoverTable <sameed.gilani@gmail.com>
2025-06-17 18:45:53 +0500

    basic game rules are working now

--- src/App.jsx
+++ src/App.jsx
@@ -6,54 +6,138 @@ import Card from "./components/Card/Card";
 
 function App() {
   const [dimension, setDimension] = useState(5);
-  const [guessStore, setGuessStore] = useState(
-    Array.from({ length: dimension }, () => Array(dimension).fill(null))
-  );
+  const [guessStore, setGuessStore] = useState({});
+  const todaysWord = ["h", "e", "l", "l", "o"];
 
   useEffect(() => {
-    setGuessStore(
-      Array.from({ length: dimension }, () => Array(dimension).fill(null))
-    );
+    const updatedStore = {};
+
+    for (let i = 0; i < dimension; i++) {
+      updatedStore[i] = {
+        entered: false,
+        row: Array(dimension).fill(null),
+        rowStatuses: Array(dimension).fill(null),
+      };
+    }
+
+    setGuessStore(updatedStore);
   }, [dimension]);
 
-  useEffect(() => {
-  
-  }, [guessStore])
-  
+  const checkAnswer = (guess = []) => {
+    const statusMap = [];
+
+    guess.forEach((letter, index) => {
+      if (todaysWord[index] === letter.toLowerCase()) {
+        statusMap.push("correct");
+      } else if (todaysWord.includes(letter.toLowerCase())) {
+        statusMap.push("maybe");
+      } else {
+        statusMap.push("wrong");
+      }
+    });
+
+    return statusMap;
+  };
 
   return (
     <>
       <div style={{ display: "flex", flexDirection: "column", gap: 10 }}>
-        {guessStore.map((guessRow) => {
+        {Object.keys(guessStore).map((row) => {
+          const currentRow = guessStore[row];
+
           return (
             <div style={{ display: "flex", flexDirection: "row", gap: 10 }}>
-              {guessRow.map((guess) => {
-                return <Card />;
+              {Array.from({ length: currentRow.row.length }).map((_, i) => {
+                return (
+                  <div
+                    tabIndex={0}
+                    onKeyDown={(event) => {
+                      const letter = event.key;
+
+                      if (letter === "Enter") {
+                        const currentGuessingRowIndex = Object.keys(
+                          guessStore
+                        ).find((key) => !guessStore[key].entered);
+
+                        const currentGuessingRow =
+                          guessStore[currentGuessingRowIndex];
+
+                        if (currentGuessingRow?.row?.every((item) => !!item)) {
+                          currentGuessingRow.entered = true;
+
+                          const statusRow = checkAnswer(currentGuessingRow.row);
+                          currentGuessingRow.rowStatuses = statusRow;
+                          setGuessStore((prev) => {
+                            return {
+                              ...prev,
+                              [currentGuessingRowIndex]: currentGuessingRow,
+                            };
+                          });
+                        }
+                      } else if (letter === "Backspace") {
+                        const currentGuessingRowIndex = Object.keys(
+                          guessStore
+                        ).find((key) => !guessStore[key].entered);
+
+                        const currentGuessingRow =
+                          guessStore[currentGuessingRowIndex];
+
+                        const lastStringIndex =
+                          currentGuessingRow.row.reduceRight(
+                            (acc, item, index) => {
+                              if (acc === -1 && typeof item === "string") {
+                                return index;
+                              }
+                              return acc;
+                            },
+                            -1
+                          );
+
+                        if (lastStringIndex >= 0) {
+                          currentGuessingRow.row[lastStringIndex] = null;
+
+                          setGuessStore((prev) => {
+                            return {
+                              ...prev,
+                              [currentGuessingRowIndex]: currentGuessingRow,
+                            };
+                          });
+                        }
+                      } else if (/^[a-zA-Z]$/.test(letter)) {
+                        const currentGuessingRowIndex = Object.keys(
+                          guessStore
+                        ).find((key) => !guessStore[key].entered);
+
+                        const currentGuessingRow =
+                          guessStore[currentGuessingRowIndex];
+
+                        const currentGuessBoxIndex =
+                          currentGuessingRow.row.indexOf(null);
+
+                        if (currentGuessBoxIndex >= 0) {
+                          currentGuessingRow.row[currentGuessBoxIndex] = letter;
+
+                          setGuessStore((prev) => {
+                            return {
+                              ...prev,
+                              [currentGuessingRowIndex]: currentGuessingRow,
+                            };
+                          });
+                        }
+                      }
+                    }}
+                  >
+                    <Card
+                      guess={currentRow?.row?.[i]}
+                      guessStatus={currentRow?.rowStatuses?.[i]}
+                    />
+                  </div>
+                );
               })}
             </div>
           );
         })}
       </div>
-
-      {/* <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p> */}
     </>
   );
 }
--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,14 +1,17 @@
-import styles from './Card.module.scss';
+import styles from "./Card.module.scss";
 
-export default function Card({ text = "", guessStatus = null }) {
+export default function Card({ guess = "", guessStatus = null }) {
   const colorMap = {
     wrong: "#3a3a3c",
     maybe: "#b59f3b",
     correct: "#538d4e",
-  }
+  };
   return (
-    <div style={{backgroundColor: colorMap[guessStatus]}} className={styles.defaultCardBody}>
-      <h2>{text}</h2>
+    <div
+      style={{ backgroundColor: colorMap[guessStatus] }}
+      className={styles.defaultCardBody}
+    >
+      <h2>{guess ? guess.toUpperCase() : ""}</h2>
     </div>
   );
 }
