27e797e92eab3cad9b2a75765c87bdd020d41db6
UnderCoverTable <sameed.gilani@gmail.com>
2025-07-17 09:46:03 +0500

    setup github pages

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/wordle-clone/",
+  base: '/wordle-clone',
   plugins: [react()],
   resolve: {
     alias: {
